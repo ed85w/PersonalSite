7fd160f76929981f8d7505c77e0a99cf2d4155da
ed85w <ed.walker@hotmail.co.uk>
2019-05-29 22:45:41 +0100

    scroll effect improved, to test further

--- script.js
+++ script.js
@@ -32,10 +32,8 @@ $(document).ready(function(){
 
 		// Scale effect on scroll to portfolio
 		var portfolioStartPOS = $('#portfolio').position().top - 400;
-		var skillsStartPos = $('.key-skills').position().top - 400;
 		var scrollPOS = $(this).scrollTop();
-		var firstTechDiv = $('.tech-div:first');
-
+		
 		if (scrollPOS >= portfolioStartPOS) {
 			$('#project1').css('transform', 'scale(1) translate3d(0,0,0)');
 			$('#project2').css('transform', 'scale(1) translate3d(0,0,0)');
@@ -43,84 +41,72 @@ $(document).ready(function(){
 			$('#project1').css('transform', 'scale(0) translate3d(0,0,0)');
 			$('#project2').css('transform', 'scale(0) translate3d(0,0,0)');
 		}
-
+		
 		// skills grid skills 'bounce in' when skills grid div is (nearly) scrolled to
-		if (scrollPOS >= skillsStartPos && !firstTechDiv.hasClass('bounceInDown')){
-			$('.skills-grid').children('div').each(function (i) {
 
-				function addKeyframe() {
-					currentDiv.addClass('bounceInDown');
-					console.log('css added to ' + currentDiv);
-				}
-				var time = i * 100;
-				var currentDiv = $(this);
-				setTimeout(addKeyframe,time);
+		$('.skills-grid').each(function(){
+			var skillsStartPos = $(this).position().top - 500;
+			var firstTechDiv = $(this).children('div').first();
 
-			});
-		};
+			if (scrollPOS >= skillsStartPos && !firstTechDiv.hasClass('bounceInDown')){
+				$(this).children('div').each(function(i){
 
+					function addKeyframe() {
+						currentDiv.addClass('bounceInDown');
+						console.log('css added to ' + currentDiv);
+					}
 
-
+					var time = i * 100;
+					var currentDiv = $(this);
+					setTimeout(addKeyframe,time);
+			
+				});
+			};
+		
+		});
 	});
+		
 
-
-	// collapse navbar after selection on mob
-	$(document).on('click','.navbar-collapse.in',function(e) {
-		if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
-    		$(this).collapse('hide').delay(180);
-		}
-	});
-
-	// email js code
-	var myform = $("form#myform");
-	myform.submit(function(event){
-		event.preventDefault();
-
-		var params = myform.serializeArray().reduce(function(obj, item) {
-     	obj[item.name] = item.value;
-     	return obj;
-  	}, {});
-
-  	var service_id = "default_service";
-  	var template_id = "template_VTGznICV";
-  	myform.find("button.btn").text("Sending...");
-  	emailjs.send(service_id,template_id,params)
+		// collapse navbar after selection on mob
+		$(document).on('click','.navbar-collapse.in',function(e) {
+			if ($(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+				$(this).collapse('hide').delay(180);
+			}
+		});
+		
+		// email js code
+		var myform = $("form#myform");
+		myform.submit(function(event){
+			event.preventDefault();
+			
+			var params = myform.serializeArray().reduce(function(obj, item) {
+				obj[item.name] = item.value;
+				return obj;
+			}, {});
+			
+			var service_id = "default_service";
+			var template_id = "template_VTGznICV";
+			myform.find("button.btn").text("Sending...");
+			emailjs.send(service_id,template_id,params)
 	  	.then(function(){ 
-			alert("Message sent! I'll get back to you as soon as possible!");
-       		myform.find("button.btn").text("Send");
-       		$('#myform')[0].reset();
-     	}, function(err) {
-	       alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
-	       myform.find("button.btn").text("Send");
+				alert("Message sent! I'll get back to you as soon as possible!");
+				myform.find("button.btn").text("Send");
+				$('#myform')[0].reset();
+			}, function(err) {
+				alert("Send email failed!\r\n Response:\n " + JSON.stringify(err));
+				myform.find("button.btn").text("Send");
 	    });
-	  return false;
+			return false;
+		});
+		
+	}); // end of doc ready
+	
+	// run keyframe animation once page has loaded
+	$(window).on('load', function() {
+		window.setTimeout(function() {
+			$('#blur-line').addClass('unblur'),
+			$('.fa-arrow-alt-circle-down').addClass('slideInDown');
+		}, 100);
 	});
-
-}); // end of doc ready
-
-
-// run keyframe animation once page has loaded
-$(window).on('load', function() {
-	window.setTimeout(function() {
-		$('#blur-line').addClass('unblur'),
-		$('.fa-arrow-alt-circle-down').addClass('slideInDown');
-	}, 100);
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
